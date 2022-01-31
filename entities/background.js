4c0e98bed4e6dd4364dbaf76d81ad0d6b1cace75
Anas Gherfal <anasgher@uw.edu>
2022-01-30 21:55:32 -0800

    started on trying to add background/trees/sun

--- entities/background.js
+++ entities/background.js
@@ -2,12 +2,45 @@ class Background {
     constructor(game, x, y, spritesheet) {
         Object.assign(this, { game, x, y });
         //this.spritesheet = ASSET_MANAGER.getAsset("./background.png");
-        
+
     };
-    
-    update(){};
-    
+
+    update() {};
+
     draw(ctx) {
         ctx.drawImage(this.spritesheet, 0, 0);
     };
 };
+
+class Tree {
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.spritesheet = ASSET_MANAGER.getAsset("")
+    }
+
+    update() {
+
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 0, 0);
+    }
+
+};
+
+class Sun {
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.spritesheet = ASSET_MANAGER.getAsset("images/sun.png");
+    }
+    update() {
+
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 50, 50, 80, 30, ths.x, ths.y);
+    }
+
+}
