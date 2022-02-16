36f3e6f72439bbb97240ef7f273f74321af78208
Anas Gherfal <anasgher@uw.edu>
2022-02-15 18:45:16 -0800

    menu/sound

--- entities/sceneElements.js
+++ entities/sceneElements.js
@@ -5,11 +5,20 @@
 // }
 
 class Scene extends Entity {
+
     constructor(game) {
         super(game, 0, 0);
         this.game.camera = this;
         this.x = 0;
-        
+
+        this.title = true;
+
+        this.menuSelect = {
+            newGame: false,
+            credits: false
+        }
+
+
 
         //UI Panels
         // this.game.addEntity(new ProgressBar(game, 100, 200, 400, 30, rgb(160,38,37)));
@@ -17,22 +26,20 @@ class Scene extends Entity {
         //this.game.addEntity(this.playerHealthBar);
 
         //Player
-        this.player = new Player(game, 400, 300);
+        this.player = new Player(game, 400, 600);
         this.game.addEntity(this.player);
-        
-        this.loadLevel( 400, 300);
 
-        //Zombie
-        //this.zombie = new Zombie(game, 200, 300);
-        //this.game.addEntity(this.zombie);
+        this.loadLevel(400, 300);
 
 
+        /*
         //Test Button
         this.testButton = new Button(game, 10, 710, 200, 50, rgba(26, 188, 156, 1.0), rgba(22, 160, 133, 1.0));
         this.testButton.onMouseClicked = function() {
             // this.game.addEntity(new DamageIndicator(game, 200, 300, "HIT!", 100));
         };
         this.game.addEntity(this.testButton);
+        */
 
         this.entityTracker = new EntityTracker(game, 10, 600, this.player);
         // this.addEntity(this.entityTracker);
@@ -41,55 +48,98 @@ class Scene extends Entity {
         //this.addEntity(new FloatingBalls(game, game.width, game.height));
 
     };
-    
-    clearEntities() { 
-        this.game.entites.forEach(function (entity) {
+
+    clearEntities() {
+        this.game.entites.forEach(function(entity) {
             entity.removeFromWorld = true;
         });
     };
-    
-    
-    loadLevel( x, y ) {
+
+    enemyWave() {
+
+        //generate 5 zombies
+        for (var i = 0; i < 5; i++) {
+            this.zombie = new Zombie(this.game, i * 200, 700);
+            this.game.addEntity(this.zombie);
+        }
+    }
+
+    loadLevel(level, x, y, title) {
         this.game.entites = [];
         this.clearEntities();
         this.x = 0;
-        
-        
+        this.level = level;
+        this.title = title;
+
+        //todo: spawn new wave after a set time?
+
+        this.enemyWave();
+
         //todo : figure out max level length
         // spawn trees based on level length
         // spawn clouds based on level length
-        
-        for (var i = 0; i < 50; i++){
-            this.background = new Tree(this.game, -80+i * 100, 170);
+
+        var platNum = Math.round(Math.random() * 20);
+
+        for (var i = 0; i < 20; i++) {
+            //coin flip to make it more random idk
+
+            if (Math.round(Math.random()) / 2 == 0) {
+                var j = Math.round(Math.random() * 100);
+                this.background = new Platform(this.game, i * 300, j + 600, 300, 10);
+                this.game.addEntity(this.background);
+            }
+        }
+
+        for (var i = 0; i < 50; i++) {
+            this.background = new Tree(this.game, -80 + i * 100, 170);
             this.game.addEntity(this.background);
         }
-        
-        for (var i = 0; i < 20; i++){
+
+        for (var i = 0; i < 20; i++) {
             this.background = new Clouds(this.game, i * 1236, 50);
             this.game.addEntity(this.background);
         }
-        
-        
+
+
         this.background = new Sun(this.game, 900, 80);
         this.game.addEntity(this.background);
-        
+
+        for (var i = 0; i < 20; i++) {
+            this.background = new Sky(this.game, i * 1236, 50);
+            this.game.addEntity(this.background);
+        }
+
         this.background = new Sky(this.game, 0, 80);
         this.game.addEntity(this.background);
 
-        
+
+        // if (music && !this.title) {
+        //     ASSET_MANAGER.pauseBackgroundMusic();
+        //     ASSET_MANAGER.playAsset(music);
+        // }
+
+
         //this.player.x = x;
         //this.player.y = y;
-        
+
         //var that = this;
         //var player = false;
         //this.game.entites.forEach(function(entity) {
         //    if(that.player === entity) player = true;
         //});
-        
+
         //if(!player) this.game.addEntity(this.player);
-        
-        
-        
+
+    };
+
+    updateAudio() {
+        var mute = document.getElementById("mute").checked;
+        var volume = document.getElementById("volume").value;
+
+        ASSET_MANAGER.muteAudio(mute);
+        ASSET_MANAGER.adjustVolume(volume);
+
     };
 
     addEntity(entity) {
@@ -106,9 +156,12 @@ class Scene extends Entity {
         if (this.x > this.player.x - camB) this.x = this.player.x - camB;
 
         //zombie following player
-        //if (this.player.x - this.zombie.x < 30) {
+        //if (this.zombie) {
+        //    if (this.player.x - this.zombie.x < 30) {
         //    this.zombie.vx = 0;
+        //    }
         //}
+
 
         //platform collistion detection
         /*
