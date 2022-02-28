572d5df2e9a2d1e17944e6bc6c2fb1dd55848a1b
Anas Gherfal <anasgher@uw.edu>
2022-02-27 23:22:34 -0800

    changes the item name and position

--- entities/items.js
+++ entities/items.js
@@ -1,48 +1,41 @@
-class Item extends Entity
-{
-    constructor(game, x, y)
-    {
+class Item extends Entity {
+    constructor(game, x, y) {
         super(game, x, y);
 
         //Properties
         this.activatable = true;
         this.floatPercent = 0
 
+        this.spritesheet = ASSET_MANAGER.getAsset("images/mashroom.png");
+
         this.tempSprite = new Rectangle(this.game, x, y - 20, 10, 10, rgba(300, 50, 50, 1));
         this.children.push(this.tempSprite);
 
-        this.text = new TextElement(game, x + 5, y - 70, "ItemName [E]");
+        this.text = new TextElement(game, x + 5, y - 70, "Health [E]");
         this.text.textAlign = "center";
         this.text.sceneElement = true;
         this.children.push(this.text);
     }
 
-    activate()
-    {
+    activate() {
         let player = this.game.sceneManager.player;
 
-        if (Math.abs(player.x - this.x) < 50 && Math.abs(player.y - this.y) < 50)
-        {
+        if (Math.abs(player.x - this.x) < 50 && Math.abs(player.y - this.y) < 50) {
             this.activatable = false;
             this.setInvisible(true);
             player.changeHealth(0.1);
-        }        
+        }
     }
 
-    update()
-    {
-        if (this.floatPercent >= 1.0)
-        {
+    update() {
+        if (this.floatPercent >= 1.0) {
             this.floatPercent = 0.0;
-        }
-        else
-        {
+        } else {
             this.floatPercent += 0.005
         }
     }
 
-    draw(ctx)
-    {
+    draw(ctx) {
         let d = 5 * Math.sin(2 * Math.PI * this.floatPercent);
         this.moveBy(0, d);
         super.draw(ctx);
